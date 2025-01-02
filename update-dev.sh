4e23a7dcbb7ee753c38b7a3ab0bd75e353e06c8f
ZacZabawa <z.zabawa175@gmail.com>
2025-01-02 15:26:19 -0700

    cont

--- update-dev.sh
+++ update-dev.sh
@@ -1,1 +1,13 @@
- 
+#!/bin/bash
+
+# Navigate to dev directory
+cd ~/collusion-dev
+
+# Stash any local changes
+git stash
+
+# Pull latest changes
+git pull origin main
+
+# Run deployment script
+./deploy-dev.sh 
